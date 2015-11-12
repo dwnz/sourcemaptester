858a9b7a907bc11d02d323b63d4ac7bed71d23d1
Daniel Wylie <daniel@mindscape.co.nz>
2015-11-12 15:56:28 +1300

    Fix port

--- app.js
+++ app.js
@@ -143,6 +143,6 @@ function GetSourceFile(sourceFileUrl, socket) {
 	});
 }
 
-http.listen(3000, function() {
-	console.log('listening on *:3000');
+http.listen(process.env.PORT || 3000, function() {
+	console.log('listening on *:' + (process.env.PORT || 3000));
 });
